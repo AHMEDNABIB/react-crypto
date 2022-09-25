2baa81876faf10f4eb08ecb4ee28c8b618a4b1a8
AHMED NABIB <nabibpallab@gmail.com>
2022-09-25 10:10:02 +0600

    custome link added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { Routes, Route, Link } from "react-router-dom";
 import Header from './components/Header/Header';
 import Homepage from './components/Homepage/Homepage';
+import Coins from './components/Coins/Coins';
+import Contact from './components/Contact/Contact';
+import About from './components/About/About';
 
 const App = () => {
   return (
@@ -10,6 +13,9 @@ const App = () => {
 		
 			<Routes>
 				<Route path="/" element={<Homepage />} />
+				<Route path="/coins" element={<Coins />} />
+				<Route path="/contact" element={<Contact />} />
+				<Route path="/about" element={<About />} />
 				
 			</Routes>
 		</div>
